f986ab7198f4521d5742087bc04ae723e8a151d4
Wilson <wilsonafs@hotmail.com>
2020-09-10 10:28:31 -0300

    subindo proejto amanda

--- js/scroll-whats.js
+++
@@ -1,9 +0,0 @@
-$(window).scroll(function() {
-   var scroll = $(window).scrollTop();
-   
-   if (scroll > 800) {
-      $('.button-whats').css("display", "block");
-   } else {
-      $('.button-whats').css("display", "none");
-   }
-});
